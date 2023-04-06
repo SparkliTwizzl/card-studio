4b078262f4a489799b44ccfa385aa504637d7940
Envy Quinn (sie/hir) | SparkliTwizzl <github@sparklitwizzl.com>
2023-04-05 23:35:18 -0600

    add resources file and auto-generated file version info class

---
+++ card-studio/Program/PreBuildOutput/ProjectVersion.cs
@@ -0,0 +1,10 @@
+//AUTO-GENERATED FILE, DO NOT EDIT; ANY CHANGES WILL BE OVERWRITTEN DURING BUILD
+
+
+namespace Hammerfrost.CardStudio.Program
+{
+	public static class ProjectVersion
+	{
+		public const string CurrentVersion = "0.0.0";
+	}
+}
